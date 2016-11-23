597114d43260736140b92e2c815c508ecf79b46a
Stijn Albers van der Linden <Stijn Albers van der Linden>
2016-11-23 16:55:22 +0100

    remove lot of unneed code

--- www/shake.js
+++ www/shake.js
@@ -1,76 +1,36 @@
 module.exports = (function () {
     "use strict";
     var shake = {};
-
     var watchId = null;
+    var options = {frequency: 100};
+    var accelerations = [];
+    var amount = 30;
 
-    var options = {
-        frequency: 100
-    };
-
-    var previousAcceleration = {
-        x: null,
-        y: null,
-        z: null
-    };
-
-    var shakings = [];
-
-    var shakeCallBack = null;
-    var sensitivity = 30;
-
-    shake.startWatch = function (_sensitivity, onError) {
-        // if (typeof (onShake) !== "function") {
-        //     return;
-        // }
-
-        if (typeof (_sensitivity) === "number") {
-            sensitivity = _sensitivity;
+    shake.startWatch = function (_amount, onError) {
+        if (typeof (amount) === "number") {
+            amount = _amount;
         }
-
-        // shakeCallBack = debounce(onShake);
 
         watchId = navigator.accelerometer.watchAcceleration(assessCurrentAcceleration, onError, options);
     };
 
-    // Stop watching the accelerometer for a shake gesture
+    // Stop watching the accelerometer and return collected data
     shake.stopWatch = function () {
         if (watchId !== null) {
             navigator.accelerometer.clearWatch(watchId);
             watchId = null;
 
-            previousAcceleration = {
-                x: null,
-                y: null,
-                z: null
-            };
-
-            return shakings;
+            return accelerations;
         }
     };
 
     // Assess the current acceleration parameters to determine a shake
     var assessCurrentAcceleration = function (acceleration) {
-        shakings.push(acceleration);
-        console.log("acceleration", acceleration);
-        console.log("datetime", new Date());
-        var accelerationChange = {};
-        if (previousAcceleration.x !== null) {
-            accelerationChange.x = Math.abs(previousAcceleration.x - acceleration.x);
-            accelerationChange.y = Math.abs(previousAcceleration.y - acceleration.y);
-            accelerationChange.z = Math.abs(previousAcceleration.z - acceleration.z);
+        accelerations.push(acceleration);
+
+        if(accelerations.length === amount) {
+            shake.stopWatch();
         }
-
-        previousAcceleration = {
-            x: acceleration.x,
-            y: acceleration.y,
-            z: acceleration.z
-        };
-
-        // if (accelerationChange.x + accelerationChange.y + accelerationChange.z > sensitivity) {
-        //     // Shake detected
-        //     shakeCallBack();
-        // }
     };
 
     return shake;
