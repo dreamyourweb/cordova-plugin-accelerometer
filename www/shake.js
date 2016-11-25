7a9e9e864503dd81e299489df4103bdab2e82701
Stijn Albers van der Linden <Stijn Albers van der Linden>
2016-11-25 10:59:16 +0100

    make producution ready

--- www/shake.js
+++ www/shake.js
@@ -4,14 +4,10 @@ module.exports = (function () {
     var watchId = null;
     var options = {frequency: 100};
     var accelerations = [];
-    var amount = 30;
 
-    shake.startWatch = function (_amount, onError) {
-        if (typeof (amount) === "number") {
-            amount = _amount;
-        }
-
+    shake.startWatch = function (onError) {
         if(watchId === null) {
+            accelerations = [];
             watchId = navigator.accelerometer.watchAcceleration(assessCurrentAcceleration, onError, options);     
         }
     };
@@ -22,18 +18,12 @@ module.exports = (function () {
             navigator.accelerometer.clearWatch(watchId);
             watchId = null;
         }
-        
-        returnData = accelerations;
-        accelerations = [];
-        console.log("accelerations " + returnData);
-        return returnData;
+        return accelerations;
     };
 
     // Assess the current acceleration parameters and push it in the return data
     var assessCurrentAcceleration = function (acceleration) {
         accelerations.push(acceleration); 
-
-        console.log("here " + accelerations.length);     
     };
 
     return shake;
